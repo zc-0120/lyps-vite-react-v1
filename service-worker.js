bb2cba1a71de6b5eff69c3f6471cd82831f7805f
Zhicheng Gong <109425to@gmail.com>
2024-10-01 01:45:04 +0800

    Updates

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+// public/service-worker.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        // 其他靜態資源
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
